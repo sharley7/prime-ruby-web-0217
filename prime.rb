146764939f7b669e65e96eceea4a25b1be982f95
S. Marie Kav <siobhan.harley@gmail.com>
2017-02-04 23:23:44 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,17 @@
-# Add  code here!
+def sieve (max)
+  list = (0..max).to_a
+  counter = 0
+  list[0] = nil
+  list[1] = nil
+  list.each do |num|
+    next unless num
+    break if num * num > max
+        counter += 1
+        (num * num).step(max,num) { |m| list[m] = nil }
+      end
+list.compact
+end
+
+def prime?(num)
+  sieve(num).include?(num)
+end
